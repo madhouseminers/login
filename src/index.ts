1e2b8834075ffcb0c0411c69c4e80694d8d0716d
Steve Goode <steve@madhouseminers.com>
2021-08-16 09:40:22 +0100

    Login now issues JWT

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { readFileSync } from "fs";
 import { resolve } from "path";
+
+import { Client } from "pg";
+export const client = new Client({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+client.connect();
 
 import { ApolloServer, gql } from "apollo-server";
 import { buildSubgraphSchema } from "@apollo/federation";
@@ -12,4 +23,6 @@ const typeDefs = gql`
 const server = new ApolloServer({
   schema: buildSubgraphSchema([{ resolvers, typeDefs }]),
 });
-server.listen({ port: 4000 }).then(({ url }) => console.log(url));
+server
+  .listen({ port: process.env.PORT || 4000 })
+  .then(({ url }) => console.log(url));
--- src/resolvers/login.ts
+++ src/resolvers/login.ts
@@ -1,4 +1,19 @@
-export default (_, { email, password }) => {
-  console.log(email, password);
-  return { email, password };
+import { verify } from "argon2";
+import * as jwt from "jsonwebtoken";
+import { client } from "../index";
+
+export default async (_, { email, password }) => {
+  const res = await client.query(
+    "select id, password from users where email=$1",
+    [email]
+  );
+  if (!res.rowCount || !(await verify(res.rows[0].password, password))) {
+    throw new Error("Invalid email or password");
+  }
+
+  return await jwt.sign({ email: email }, process.env.JWT_SIGNING_KEY, {
+    expiresIn: "1h",
+    subject: res.rows[0].id.toString(),
+    notBefore: "0s",
+  });
 };
